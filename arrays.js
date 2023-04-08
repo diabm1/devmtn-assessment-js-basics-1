0528c4a6c2a5c0ca093943ff4fdc1893a3cbc140
diabm1 <diabm1855@gmail.com>
2023-04-08 16:10:59 -0400

    finished arrays.js

--- arrays.js
+++ arrays.js
@@ -1,14 +1,18 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a copy of the faveColors array called 'colorCopy' using the slice method. */
 
-const faveColors = ['red', 'green', 'black']
+const faveColors = ["red", "green", "black"];
 
 //CODE HERE
+const colorCopy = faveColors.slice();
+console.log(colorCopy);
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+colorCopy.push("purple");
+console.log(colorCopy);
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -16,21 +20,42 @@ const faveColors = ['red', 'green', 'black']
   numbers array that will capture only the middle numbers (2, 3, 4).
 */
 
-const numbers = [1, 2, 3, 4, 5]
+const numbers = [1, 2, 3, 4, 5];
 
 //CODE HERE
-
+const middleNums = numbers.splice(1, 3);
+console.log(middleNums);
 
 //////////////////PROBLEM 4////////////////////
 /* 
-  Create an empty array called 'answers'.
+  Create an empty array called 'answers'. ✅
   Using a for loop, loop over the array called 'bigOrSmallArray', and check to see if the 
-  number in the array is GREATER than 100. 
-  If it is, push 'big' as a string to the answers array. 
+  number in the array is GREATER than 100. ✅
+  If it is, push 'big' as a string to the answers array. ✅
   If the number is LESS than or EQUAL to 100, push 'small' as a string 
   to the answers array. 
 */
 
-const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
+const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352];
 
 // CODE HERE
+let answers;
+
+// for (let i = 0; i < bigOrSmallArray.length; i++) {
+//   if(bigOrSmallArray[i] > 100){
+//     bigOrSmallArray.push('big')
+//   } else if(bigOrSmallArray[i] <= 100) {
+//     bigOrSmallArray.push('small')
+//   }
+// }
+
+// returns same array but with different datatypes
+for (let i = 0; i < bigOrSmallArray.length; i++) {
+  if (bigOrSmallArray[i] > 100) {
+    bigOrSmallArray.splice(i, 1, "big");
+  } else if (bigOrSmallArray[i] <= 100) {
+    bigOrSmallArray.splice(i, 1, "small");
+  }
+}
+
+console.log(bigOrSmallArray);
