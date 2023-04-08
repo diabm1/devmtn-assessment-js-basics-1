48b0ea725887086a23e26d6ad2f9fe6854ae071b
diabm1 <diabm1855@gmail.com>
2023-04-08 15:56:39 -0400

    adding changes I've made so far

---
+++ sdlc.js
@@ -0,0 +1,29 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+
+const analysisOfRequirements = "This is the second step, in which the team performs a detailed analysis of the requirements. The analysis helps identify any potential challenges or obstacles that may arise during development."
+
+const design = "This is the third step, in which the team creates a detailed design of the software. This includes creating a technical architecture, defining the user interface, and designing the database schema."
+
+const implementation = "This is the fourth step, in which the software is developed accroding to the design created in the previous stage. This involves writing code, testing it, and debugging any issues."
+
+const testingAndIntegration = "This is the fifth and a critical step in the SDLC. Testing ensures the software works and meets requirements, while integration combines components/modules to make a single system and tests their interaction and communication to function as expected, which can be complex when dealing with many components or external systems."
